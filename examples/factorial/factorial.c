b204a47394be37da231f30d89919663997c9b599
Alexander Baygeldin <a.baygeldin@gmail.com>
2016-08-08 06:56:46 +0300

    improve factorial example

--- examples/factorial/factorial.c
+++ examples/factorial/factorial.c
@@ -1,15 +1,29 @@
+/**
+ * This is just a simple example of recursion.
+ * It has a simple implementation and a tail call optimization.
+ * It's also possible to pass parameters to it through CLI.
+ */
+
+
+#include <stdlib.h>
 #include <stdio.h>
 
+#define NUM 10
+
 long factorial(int);
+long factorial_tail_call(int, int);
 
-int my_global = 10;
-
-int main() {
-  long res = factorial(my_global);
-  printf("%d! = %ld\n", my_global, res);
+int main(int argc, char **argv) {
+  int num = argc > 1 ? atoi(argv[1]) : NUM;
+  printf("%d! = %ld (simple)\n", num, factorial(num));
+  printf("%d! = %ld (tail call)\n", num, factorial_tail_call(1, num));
   return 0;
 }
 
 long factorial(int n) {
   return n == 0 ? 1 : n * factorial(n - 1);
+}
+
+long factorial_tail_call(int acc, int n) {
+  return n == 0 ? acc : factorial_tail_call(n * acc, n - 1);
 }
