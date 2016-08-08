5fd992111ecd745560a7ea90db7c93df62ef5cc1
Alexander Baygeldin <a.baygeldin@gmail.com>
2016-08-08 06:26:42 +0300

    fix tickets example

--- examples/tickets/tickets.c
+++ examples/tickets/tickets.c
@@ -8,42 +8,58 @@
 #define TICKETS_COUNT 100
 #define SELLERS_COUNT 5
 
+typedef struct {
+  int id;
+} spawn_seller_args;
+
+// Initializing globals.
 static int tickets = TICKETS_COUNT;
 static pthread_mutex_t lock;
 
-void* sell_tickets();
+void* spawn_seller(void *args);
 void find_customer();
 
 int main() {
+  srand(time(0));
+
+  // Initializing locals.
   int i;
   pthread_t sellers[SELLERS_COUNT];
 
-  if (pthread_mutex_init(&lock, NULL) != 0) return -1;
+  // Initializing the mutex.
+  if (pthread_mutex_init(&lock, 0) != 0) return -1;
 
-  srand(time(NULL));
-
+  // Running threads.
   for (i = 0; i < SELLERS_COUNT; i++) {
-    int status = pthread_create(&(sellers[i]), NULL,
-      &sell_tickets, NULL);
+    spawn_seller_args *args = malloc(sizeof(*args));
+    args->id = i;
+    int status = pthread_create(&(sellers[i]), 0, &spawn_seller, args);
     if (status != 0) return -1;
   }
 
+  // Waiting for threads to finish.
   for (i = 0; i < SELLERS_COUNT; i++) {
-    pthread_join(sellers[i], NULL);
+    pthread_join(sellers[i], 0);
   }
 
+  // Destroying the mutex.
   pthread_mutex_destroy(&lock);
 
   return 0;
 }
 
-void* sell_tickets() {
+void* spawn_seller(void *args) {
+  spawn_seller_args *seller_args = args;
+
   bool done = false;
   int tickets_sold = 0;
-  int seller_id = rand() % 100;
+  int id = seller_args->id;
 
   while (!done) {
+    // Doing some time-consuming work.
     find_customer();
+
+    // Selling the ticket.
     pthread_mutex_lock(&lock);
 
     if (tickets == 0) {
@@ -52,13 +68,15 @@ void* sell_tickets() {
       tickets--;
       tickets_sold++;
       printf("%i seller sold  %i tickets, %i remain\n",
-        seller_id, tickets_sold, tickets);
+        id, tickets_sold, tickets);
     }
 
     pthread_mutex_unlock(&lock);
   }
 
-  return NULL;
+  free(seller_args);
+
+  return 0;
 }
 
 void find_customer() {
